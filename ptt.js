aa730d952118d53e6330e93505e67d49fae3c613
Malte Schmitz <schmitz@isp.uni-luebeck.de>
2020-07-23 19:48:01 +0200

    Add color switcher

--- ptt.js
+++ ptt.js
@@ -1,10 +1,10 @@
-(function() {
+(function () {
   let AudioContext = window.AudioContext || window.webkitAudioContext;
 
   let analyser;
   const initialize = () => {
     if (analyser) return;
-    navigator.mediaDevices.getUserMedia({audio: true}).then(stream => {
+    navigator.mediaDevices.getUserMedia({ audio: true }).then(stream => {
       const audioContext = new AudioContext();
       const input = audioContext.createMediaStreamSource(stream);
       analyser = audioContext.createAnalyser();
@@ -24,12 +24,12 @@
     }
   }
 
-  const bars = [12.56640625,11.70703125,13.107421875,64.16015625,77.431640625,75.91796875,54.48046875,29.9921875,13.5703125,4.673828125,2.58984375,11.0546875,10.15234375,67.25390625,57.966796875,35.634765625,16.876953125,13.265625,12.08203125,13.7734375,68.455078125,68.0546875,67.654296875,45.087890625,25.474609375,13.958984375,13.458984375,9.0390625,5.984375,12.1875];
+  const bars = [12.56640625, 11.70703125, 13.107421875, 64.16015625, 77.431640625, 75.91796875, 54.48046875, 29.9921875, 13.5703125, 4.673828125, 2.58984375, 11.0546875, 10.15234375, 67.25390625, 57.966796875, 35.634765625, 16.876953125, 13.265625, 12.08203125, 13.7734375, 68.455078125, 68.0546875, 67.654296875, 45.087890625, 25.474609375, 13.958984375, 13.458984375, 9.0390625, 5.984375, 12.1875];
   const processInput = () => {
     if (analyser && active) {
       const array = new Uint8Array(analyser.frequencyBinCount);
       analyser.getByteFrequencyData(array);
-      const sum = array.reduce((a,b) => a + b);
+      const sum = array.reduce((a, b) => a + b);
       const average = sum / array.length;
       bars.push(average);
       if (bars.length > 30) {
@@ -73,5 +73,58 @@
   recordButton.addEventListener('mouseup', () => {
     buttonHead.removeAttribute('transform');
   });
+
+  const colors = [
+    {
+      gradient: [
+        { offset: 0, color: "#4D99B2" },
+        { offset: 0.3765, color: "#56A5BA" },
+        { offset: 1, color: "#6DC4CE" }
+      ],
+      head: "#1A8097"
+    }, {
+      gradient: [
+        { offset: 0, color: "#fe2b20" },
+        { offset: 0.7, color: "#fd5d54" },
+        { offset: 1, color: "#ec6058" }
+      ],
+      head: "#bb1c14"
+    }, {
+      gradient: [
+        { offset: 0, color: "#028d0d" },
+        { offset: 0.7, color: "#04ad12" },
+        { offset: 1, color: "#00ad2e" },
+      ],
+      head: "#00770b"
+    }, {
+      gradient: [
+        { offset: 0, color: "#6f6397" },
+        { offset: 0.7, color: "#695994" },
+        { offset: 1, color: "#8377ac" },
+      ],
+      head: "#57478d"
+    }, {
+      gradient: [
+        { offset: 0, color: "#eb9b19" },
+        { offset: 0.3765, color: "#feaf09" },
+        { offset: 1, color: "#ffb93a" },
+      ],
+      head: "#e98f11"
+    }
+  ];
+  let colorIndex = 0;
+
+  const nextColorButton = document.querySelector('#nextColor button');
+  nextColorButton.addEventListener('click', () => {
+    colorIndex = (colorIndex + 1) % colors.length;
+    const color = colors[colorIndex];
+    const gradient = svg.getElementById('gradient');
+    gradient.querySelectorAll('stop').forEach((stop, i) => {
+      stop.setAttribute('offset', color.gradient[i].offset);
+      stop.setAttribute('stop-color', color.gradient[i].color);
+    });
+    buttonHead.setAttribute('fill', color.head);
+  });
+
 })();
 
